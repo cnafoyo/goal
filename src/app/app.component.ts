7c6b262fc38d04e31862c6db2fa92fafb5acc133
Billy Chepkemoi <cnafayo@gmail.com>
2020-10-01 06:24:42 +0300

    Goal class added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goal = 'Watch Finding Nemo'
+  goals: Goal[] = [
+    { id: 1, name: 'Watch Finding Nemo' },
+    { id: 2, name: 'Buy cookies' },
+    { id: 3, name: 'Get new phone case' },
+    { id: 4, name: 'Get Dog food' },
+    { id: 5, name: 'Solve math homework' },
+    { id: 6, name: 'Plot my world nomination plan' }
+
+  ];
 }
