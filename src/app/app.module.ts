757462c0a13f82fc3a766dead74d8fff1edba045
Billy Chepkemoi <cnafayo@gmail.com>
2020-10-04 15:04:28 +0300

    additional component detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponentComponent } from './goal-detail-component/goal-detail-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/goal-detail-component/goal-detail-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-detail-component',
+  templateUrl: './goal-detail-component.component.html',
+  styleUrls: ['./goal-detail-component.component.css']
+})
+export class GoalDetailComponentComponent implements OnInit {
+  @Input() goal: Goal;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
